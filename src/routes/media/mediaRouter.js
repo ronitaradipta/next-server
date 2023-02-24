61f1abfabb249cd27c836c65a70913c6fac9c2a3
rivabayu <rivabays@gmail.com>
2023-02-24 20:33:40 +0700

    mencoba firebase

--- src/routes/media/mediaRouter.js
+++ src/routes/media/mediaRouter.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const mediaControler = require('../../controllers/media/media');
 // change this :
-// const format = require('../../middleware/format');
-// const upload = require('../../middleware/upload');
+const format = require('../../middleware/format');
+const upload = require('../../middleware/upload');
 
 // and replace with :
-const { FileUpload, FileResize } = require('../../middleware/Media');
+// const { FileUpload, FileResize } = require('../../middleware/Media');
 const router = express.Router();
 
 router.post(
   '/upload',
-  FileUpload.single('file'),
-  FileResize,
+  upload.single('file'), 
+  format,
   mediaControler.addMedia
 );
 router.get('/', mediaControler.getAllMedia);
@@ -19,8 +19,8 @@ router.delete('/:id', mediaControler.deleteMedia);
 router.get('/:id', mediaControler.getMediaByID);
 router.put(
   '/:id',
-  FileUpload.single('file'),
-  FileResize,
+  upload.single('file'),
+  // FileResize,
   mediaControler.updateMedia
 );
 
