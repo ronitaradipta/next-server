cb778ee6974b62aaf13d0185088c186318a6e450
Fiqri Oemry <121015122+FiqriReomy@users.noreply.github.com>
2023-03-02 11:26:57 +0700

    adding template and route for token send,verify + fix code

--- src/controllers/Authenticate/ResetPassword.js
+++ src/controllers/Authenticate/ResetPassword.js
@@ -5,12 +5,11 @@ const { Op } = require("sequelize");
 module.exports = async (req, res) => {
   try {
     const resetToken = req.params.token;
-
     // check reset token
     const user = await resetPassword.findOne({
       where: { resetToken: resetToken, ExpiresAt: { [Op.gte]: new Date() } },
     });
-    if (!user) return res.status(200).send({ msg: "Reset Password Token is invalid or has been expired" });
+    if (!user) return res.status(400).send({ msg: "Reset Password Token is invalid or has been expired" });
 
     const { newPassword, newPasswordConfirm } = req.body;
     // check password match
---
+++ src/utils/emailTemplate.js
@@ -0,0 +1,149 @@
+function getTemplate(options) {
+  return `<div class="">
+    <div class="aHl"></div>
+    <div id=":pg" tabindex="-1" class="ajV">
+      <div id=":ov" class="ajR" role="button" tabindex="0" aria-label="Sembunyikan konten yang diperluas" aria-expanded="true" data-tooltip="Sembunyikan konten yang diperluas">
+        <img class="ajT" src="//ssl.gstatic.com/ui/v1/icons/mail/images/cleardot.gif" />
+      </div>
+    </div>
+    <div id=":ml" class="ii gt" jslog="20277; u014N:xr6bB; 4:W251bGwsbnVsbCxbXV0.">
+      <div id=":tf" class="a3s aiL msg7666649528522313186 adM">
+        <div class="HOEnZb">
+          <div class="adm">
+            <div id="q_42" class="ajR h4" data-tooltip="Sembunyikan konten yang diperluas" aria-label="Sembunyikan konten yang diperluas" aria-expanded="true"><div class="ajT"></div></div>
+          </div>
+          <div class="im">
+            <u></u>
+  
+            <div style="height: 100%; margin: 0; font-family: 'Nunito Sans', Helvetica, Arial, sans-serif; background-color: #f4f4f7; color: #51545e; width: 100% !important">
+              <table width="100%" cellpadding="0" cellspacing="0" role="presentation" style="width: 100%; margin: 0; padding: 0; background-color: #f4f4f7">
+                <tbody>
+                  <tr>
+                    <td align="center" style="font-family: 'Nunito Sans', Helvetica, Arial, sans-serif; font-size: 14px">
+                      <table width="100%" cellpadding="0" cellspacing="0" role="presentation" style="width: 100%; margin: 0; padding: 0">
+                        <tbody>
+                          <tr>
+                            <td style="font-family: 'Nunito Sans', Helvetica, Arial, sans-serif; font-size: 14px; padding: 25px 0; text-align: center"></td>
+                          </tr>
+                          <tr>
+                            <td width="100%" cellpadding="0" cellspacing="0" style="font-family: 'Nunito Sans', Helvetica, Arial, sans-serif; font-size: 14px; width: 100%; margin: 0; padding: 0; background-color: #f4f4f7">
+                              <table class="m_7666649528522313186email-body_inner" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation" style="width: 580px; margin: 0 auto; padding: 0; background-color: #ffffff">
+                                <tbody>
+                                  <tr>
+                                    <td style="font-family: 'Nunito Sans', Helvetica, Arial, sans-serif; font-size: 14px; padding: 25px 0; text-align: center">
+                                      <a
+                                        href=${process.env.BASE_URL}
+                                        style="font-size: 16px; font-weight: bold; color: #a8aaaf; text-decoration: none"
+                                        target="_blank""
+                                      >
+                                        <input type="hidden" value="" />
+                                        <img src="https://res.cloudinary.com/dnlrzhdcs/image/upload/v1677511653/logo_fsbinu.png" alt="nextcommerce" width="250" style="border: none" class="CToWUd" data-bit="iit" />
+                                      </a>
+                                      <hr style="margin-top: 30px; border: 5px solid #f4f4f7" />
+                                    </td>
+                                  </tr>
+                                  <tr>
+                                    <td style="font-family: 'Nunito Sans', Helvetica, Arial, sans-serif; font-size: 14px; padding: 35px">
+                                      <div>
+                                        <h2 style="margin-top: 0; text-transform: capitalize; color: #333333; font-size: 16px; font-weight: bold; text-align: left">Halo, ${options.user.name}</h2>
+                                        <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0.4em 0 1.1875em; line-height: 1.625; color: #51545e">
+                                          Kamu meminta penggantian kata sandi dari akun
+                                          <a href="#" target="_blank">nextcommerce.com</a>
+                                          mu. Ubah kata sandi mu dengan <b>menekan tombol</b> di bawah ini:
+                                        </p>
+                                        <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; width: 100%; box-sizing: border-box">
+                                          <tbody>
+                                            <tr>
+                                              <td align="left" style="font-family: 'Nunito Sans', Helvetica, Arial, sans-serif; font-size: 14px; vertical-align: top; padding-bottom: 15px">
+                                                <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; width: auto">
+                                                  <tbody>
+                                                    <tr>
+                                                      <td style="font-family: 'Nunito Sans', Helvetica, Arial, sans-serif; font-size: 14px; vertical-align: top; background-color: #3498db; border-radius: 5px; text-align: center">
+                                                        <a
+                                                          href="${options.passwordResetURL}"
+                                                          class="m_7666649528522313186button"
+                                                          style="
+                                                            background-color: #3869d4;
+                                                            border-top: 10px solid #3869d4;
+                                                            border-right: 18px solid #3869d4;
+                                                            border-bottom: 10px solid #3869d4;
+                                                            border-left: 18px solid #3869d4;
+                                                            display: inline-block;
+                                                            color: #ffffff;
+                                                            text-decoration: none;
+                                                            border-radius: 3px;
+                                                            box-sizing: border-box;
+                                                          "
+                                                          target="_blank"
+                                                          >Ubah Kata Sandi</a
+                                                        >
+                                                      </td>
+                                                    </tr>
+                                                  </tbody>
+                                                </table>
+                                              </td>
+                                            </tr>
+                                          </tbody>
+                                        </table>
+  
+                                        <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0.4em 0 1.1875em; line-height: 1.625; color: #51545e">
+                                          Terimakasih telah mempercayai
+                                          <a href="#" target="_blank">nextcommerce.com</a>
+                                        </p>
+                                        <div style="margin-top: 50px"><hr /></div>
+                                        <span style="font-size: 12px">Email ini dikirim secara otomatis, Kamu tidak perlu membalasnya</span>
+                                      </div>
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td style="font-family: 'Nunito Sans', Helvetica, Arial, sans-serif; font-size: 14px">
+                              <table class="m_7666649528522313186email-footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation" style="width: 570px; margin: 0 auto; padding: 0; text-align: center">
+                                <tbody>
+                                  <tr>
+                                    <td align="center" style="font-family: 'Nunito Sans', Helvetica, Arial, sans-serif; font-size: 14px; padding: 35px">
+                                      <p style="margin: 0.4em 0 1.1875em; font-size: 13px; line-height: 1.625; color: #6b6e76; text-align: center">
+                                        © 2023
+                                        <a href="#" target="_blank">nextcommerce.com</a>. All rights reserved.
+                                      </p>
+                                      <p style="margin: 0.4em 0 1.1875em; font-size: 13px; line-height: 1.625; color: #6b6e76; text-align: center">
+                                        Team Project 2 & 3
+                                        <br />Indonesia
+                                      </p>
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+  
+              <img
+                src="https://ci4.googleusercontent.com/proxy/okUPBE8QUErdFE_ZbRUbDMl3j9w8hAR8N-Iimup-VFT1rqE1H8k3a7ldNeVurfe2RxIqtyfUQgvPHgT_iwTeOkrLeokkKKAsde6lO6D5P7LcaE3Qx4V3lk-Yz2YwStPdmjQbJ6fwMtUMblVTf9HkifE3CEZB13tStmp9vb4imtCOqS-MCYJmDt5rDEu3EHMoLs3TzvcfAh0Rd3S-XB5R6_l3O7yR68aoKYtHG19mVyZ7aWpU9vriUMdjuxOA_Yebsb8gwiDdDyiORRORG03NhNzeyTFtPM4HTEAxwbu6uP9VqBzvKMNRzVlMj8XGyxje5-aEVYb-2hrNtV3A0lSc7amrksR2bG7qnOZvJW5CZ9uqQ9HlQHgVF9YtJ1LvKxjQUOsJXZEfJNP4_gdHLim5MehPI1epmlgXXMtdMlHAyI6vQoIk0Utwj6YuZNrTe1NoCDRUAfe7V4F8_1MFU5SElvEZER9fVJHKpjLsDCA5mSItzgznzblWqNryLsx0mP9enHMoueQsMsKbR3h3Xxt6gR51RTSyIZS7kG4KfJXmoKElVDbL0a2imwtdkiIifLLuGWWMd5yurBr9VdLCUKRq7P9J6S-TLrpTzYZoUrACKHKmL9qh54n22odsQabnZvNL2LU7I-EDxiBCx8S1OGibIh7WHjLnI4I=s0-d-e1-ft#https://ea.pstmrk.it/open?m=v3_1.cMfwrmXDKrkKkzN80HubGQ.8V7rHLarmH9V6m5xWf1Hv505NNl3VRcU4xg_XSnvHhb88-9839O18s9hnyJdwy883EmKKal2AGnpy_fxuXJAH3aNPsxOPHX4cH734tliKwyXgLfP4__ZRJtAiHL_E9wDmZq_wp48DFsL-XBeRWqeFHUWd2GL4AtRy1-hvi0YNyuUGENNaqpiAR7TUHWdnzLekr6ETp8cIchzdD70S3WazYVjj10UvxlDxbxDSd83NUlUsSvhMSYgGhDcrNJXfCmpnL2G7cYny4shvton3OvrOZpifkO0ag-BucQ6y0Xe6DSvt4uAobSVcnDq6x5n8zM_W7lM6gI3Lb-0MT-fB4T8BEnhj0-V_S6Ph1ozmrz7uvDV3mU6AysMxJBDWbcb9NqtgxtUG43qO8n4M3EMBqVbzlkjc_Gyhkqj1_q0FuzxzOE"
+                width="1"
+                height="1"
+                border="0"
+                alt=""
+                class="CToWUd"
+                data-bit="iit"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id=":r3" class="ii gt" style="display: none"><div id=":s0" class="a3s aiL"></div></div>
+    <div class="hi"></div>
+  </div>
+  `;
+}
+
+module.exports = getTemplate;
