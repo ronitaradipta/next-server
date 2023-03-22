9d3204b8945908cea72b62580b3e63ec78c14903
ronitaradipta <taradipta.roni@gmail.com>
2023-03-22 15:36:55 +0800

    fix chrome login issue, store token to header, but refresh token not worked yet

--- src/routes/user/AuthRoute.js
+++ src/routes/user/AuthRoute.js
@@ -5,9 +5,10 @@ const router = express.Router();
 
 router.post('/register', AuthController.Register);
 router.post('/login', AuthController.Login);
-router.get('/logout', AuthController.Logout);
+router.post('/logout', AuthController.Logout);
 router.post('/verify-otp', AuthController.VerifyOtp);
 router.post('/forgot', AuthController.ForgotPassword);
 router.get('/verify/:token', AuthController.verifyToken);
 router.put('/reset/:token', AuthController.ResetPassword);
+router.get('/refresh-token', AuthController.RefreshToken);
 module.exports = router;
