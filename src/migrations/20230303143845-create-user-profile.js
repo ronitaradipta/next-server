e98dc490e20dd1e80a6d60d98c25d1eaceb639c2
Fiqri Oemry <121015122+FiqriReomy@users.noreply.github.com>
2023-03-23 23:35:15 +0700

    fix bugs

--- src/migrations/20230303143845-create-user-profile.js
+++ src/migrations/20230303143845-create-user-profile.js
@@ -11,6 +11,10 @@ module.exports = {
       },
       userId: {
         type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
       },
       birth_day: {
         type: Sequelize.STRING,
